c695a341d5c514c685327bab776c971f86b98203
Kentucky94 <lis5043@gmail.com>
2020-01-10 06:56:07 +0600

    init

---
+++ src/components/Lock/Lock.js
@@ -0,0 +1,51 @@
+import './Lock.css';
+
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+class Lock extends Component {
+  render() {
+    return (
+      <div className='Lock'>
+        <p className="display">{this.props.value}</p>
+        <button onClick={this.props.addSeven}>7</button>
+        <button onClick={this.props.addEight}>8</button>
+        <button onClick={this.props.addNine}>9</button>
+        <button onClick={this.props.addFour}>4</button>
+        <button onClick={this.props.addFive}>5</button>
+        <button onClick={this.props.addSix}>6</button>
+        <button onClick={this.props.addOne}>1</button>
+        <button onClick={this.props.addTwo}>2</button>
+        <button onClick={this.props.addThree}>3</button>
+        <button onClick={this.props.deleteLast}>&#60;</button>
+        <button onClick={this.props.addZero}>0</button>
+        <button onClick={this.props.confirmCode}>E</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    value: state.value,
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addOne: () => dispatch({type: 'ADD_ONE'}),
+    addTwo: () => dispatch({type: 'ADD_TWO'}),
+    addThree: () => dispatch({type: 'ADD_THREE'}),
+    addFour: () => dispatch({type: 'ADD_FOUR'}),
+    addFive: () => dispatch({type: 'ADD_FIVE'}),
+    addSix: () => dispatch({type: 'ADD_SIX'}),
+    addSeven: () => dispatch({type: 'ADD_SEVEN'}),
+    addEight: () => dispatch({type: 'ADD_EIGHT'}),
+    addNine: () => dispatch({type: 'ADD_NINE'}),
+    addZero: () => dispatch({type: 'ADD_ZERO'}),
+    deleteLast: () => dispatch({type: 'DELETE_LAST'}),
+    confirmCode: () => dispatch({type: 'CONFIRM_CODE'}),
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Lock);
---
+++ src/store/reducer.js
@@ -0,0 +1,53 @@
+const initialState = {
+  combination: 9486,
+  value: '',
+};
+
+const reducer = (state = initialState, action) => {
+  if(action.type === 'ADD_ONE' && state.value.length < 4){
+    return {...state, value: state.value + 1}
+  }
+  if(action.type === 'ADD_TWO' && state.value.length < 4){
+    return {...state, value: state.value + 2}
+  }
+  if(action.type === 'ADD_THREE' && state.value.length < 4){
+    return {...state, value: state.value + 3}
+  }
+  if(action.type === 'ADD_FOUR' && state.value.length < 4){
+    return {...state, value: state.value + 4}
+  }
+  if(action.type === 'ADD_FIVE' && state.value.length < 4){
+    return {...state, value: state.value + 5}
+  }
+  if(action.type === 'ADD_SIX' && state.value.length < 4){
+    return {...state, value: state.value + 6}
+  }
+  if(action.type === 'ADD_SEVEN' && state.value.length < 4){
+    return {...state, value: state.value + 7}
+  }
+  if(action.type === 'ADD_EIGHT' && state.value.length < 4){
+    return {...state, value: state.value + 8}
+  }
+  if(action.type === 'ADD_NINE' && state.value.length < 4){
+    return {...state, value: state.value + 9}
+  }
+  if(action.type === 'ADD_ZERO' && state.value.length < 4){
+    return {...state, value: state.value + 0}
+  }
+  if(action.type === 'CONFIRM_CODE' && state.value.length === 4){
+    if(state.value === String(state.combination)){
+      return {...state, value: 'ACCESS GRANTED'}
+    }else{
+      return {...state, value: 'ACCESS DENIED'}
+    }
+  }
+  if(action.type === 'DELETE_LAST'){
+    let newValue = state.value;
+    const res = newValue.slice(0, newValue.length - 1);
+
+    return{...state, value: res};
+  }
+  return state;
+};
+
+export default reducer;
